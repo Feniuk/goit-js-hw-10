1459d79ad3bcdf977172fbc3e3f6aa80e824c431
Feniuk <143640539+Feniuk@users.noreply.github.com>
2024-01-26 14:44:27 +0100

    update

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,24 +1,39 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
-const id = document.querySelector('#datetime-picker', flatpickr);
+const startBtn = document.querySelector('button',)
+startBtn.addEventListener('click',)
 
-id.window.addEventListener('load', function () {
-    flatpickr("#myPickerId", {
-        enableTime: true, time_24hr: true, defaultDate: new Date(), minuteIncrement: 1, onClose(selectedDates) {
-            console.log(selectedDates[0]);
-}, });
-});
-id.flatpickr(selector, options)
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(datetime-picker, options)
+
+// const id = document.querySelector('#datetime-picker');
+
+// id.window.addEventListener('load', function () {
+//     flatpickr("#myPickerId", {
+//         enableTime: true, time_24hr: true, defaultDate: new Date(), minuteIncrement: 1, onClose(selectedDates) {
+//             console.log(selectedDates[0]);
+// }, });
+// });
+// id.flatpickr(selector, options)
+// const options = {
+//     enableTime: true,
+//     time_24hr: true,
+//     defaultDate: new Date(),
+//     minuteIncrement: 1,
+//     onClose(selectedDates) {
+//       console.log(selectedDates[0]);
+//     },
+//   };
 // {
 //     altInput: true,
 //     altFormat: "F j, Y",
