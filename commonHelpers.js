8bfccceaf18d8ef0101c21233fbb9607968db806
Feniuk <143640539+Feniuk@users.noreply.github.com>
2024-01-26 13:44:44 +0000

    Deploying to gh-pages from @ Feniuk/goit-js-hw-10@1459d79ad3bcdf977172fbc3e3f6aa80e824c431 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/styles-5fac4f8b.js";import{f as t}from"./assets/vendor-992cd48f.js";const n=document.querySelector("#datetime-picker",t);n.window.addEventListener("load",function(){t("#myPickerId",{enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}})});n.flatpickr(selector,o);const o={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};
+import"./assets/styles-9bdb4e0f.js";import{f as t}from"./assets/vendor-992cd48f.js";const n=document.querySelector("button");n.addEventListener("click");const o={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};t(datetime-picker,o);
 //# sourceMappingURL=commonHelpers.js.map
